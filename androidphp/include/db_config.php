f4495decba0d542c33d6152e5327a636a1afeb94
Gaddiel Technologies Private Limited <github@gaddieltech.com>
2016-03-28 21:07:00 +0530

    First Commit

--- androidphp/include/db_config.php
+++ androidphp/include/db_config.php
@@ -2,8 +2,8 @@
 /*
  * All database connection variables
  */
-define('DB_USER', "gadfort_jpb"); // db user  gadfort_jpb
-define('DB_PASSWORD', "forAug1511"); // db password (mention your db password here) forAug1511
-define('DB_DATABASE', "gadfort_jpb-hawki-prod"); // database name   gadfort_jpb-hawki-prod
-define('DB_SERVER', "mysql3000.mochahost.com"); // db server  mysql3000.mochahost.com
+define('DB_USER', "root"); // db user  
+define('DB_PASSWORD', ""); // db password (mention your db password here) 
+define('DB_DATABASE', "hawki"); // database name   
+define('DB_SERVER', "localhost"); // db server  
 ?>
