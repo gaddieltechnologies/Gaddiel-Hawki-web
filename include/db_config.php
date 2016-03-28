0c789129073d799898f20a82283b94439cee6ef0
Gaddiel Technologies Private Limited <github@gaddieltech.com>
2016-03-28 21:10:21 +0530

    First Commit

--- include/db_config.php
+++ include/db_config.php
@@ -2,8 +2,8 @@
 /*
  * All database connection variables
  */
-define('DB_USER', "jpbsrmsys"); // db user
-define('DB_PASSWORD', "c@v@22July14"); // db password (mention your db password here)
-define('DB_DATABASE', "jpb_hawki"); // database name
-define('DB_SERVER', "gaddieltech.fatcowmysql.com"); // db server
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE', "hawki"); // database name
+define('DB_SERVER', "localhost"); // db server
 ?>
